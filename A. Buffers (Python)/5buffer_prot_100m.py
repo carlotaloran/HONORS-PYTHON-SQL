c1d11053a4da390bf2f1bffa6206ec8e0faa5c8e
carlotaloran <cloranlo@oberlin.edu>
2026-01-23 16:14:58 +0000

    ucs

--- A. Buffers (Python)/5buffer_prot_100m.py
+++ A. Buffers (Python)/5buffer_prot_100m.py
@@ -19,7 +19,7 @@ def delete_shapefile(path):
 
 # Path and buffer list definitions
 cd = "/zfs/students/cloranlo/Downloads/CREDIT_DEFOREST/DATA/DATA_CLEAN/CREDIT/GLEBAS/FARMS/100M_BUFFERS/"
-protected = "/zfs/students/cloranlo/Downloads/CREDIT_DEFOREST/DATA/SHAPEFILES/Brazil_UCS/CREDIT_DEFOREST_brazil_UCS_fixed.shp"
+protected = "/zfs/students/cloranlo/Downloads/CREDIT_DEFOREST/DATA/SHAPEFILES/brazil_UCS_fixed.shp"
 buffers = [
     "b0",
     "b100",
